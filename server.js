be6ef0ecd3f6d049b98aad1d940ffe66a37f4637
Gyuwon <gyuwon@live.com>
2014-01-13 21:57:39 +0900

    config.js

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 /* jshint node: true, laxcomma: true */
 
-var path = require('path')
+var config = require('./config')
   , util = require('util')
   , express = require('express')
   , app = express()
   , framework = require('express-play')({ app: app });
 
 app.configure(function () {
-  app.use(express.static(path.join(__dirname, '/dist')));
+  app.use(express.static(config.static));
 });
 
 app.get('/', function (req, res) {
   res.render('index.html');
 });
 
-framework.play(3000, function () {
-  util.log('Listening on ' + 3000);
+framework.play(config.port, function () {
+  util.log('Listening on ' + config.port);
 });
