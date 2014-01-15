210ef7d62b496478a7e25d6bb3038f14e7ad55e3
Yun YoungSik <ysyun@yuwin.co.kr>
2014-01-14 21:16:59 -0500

    add ui-router menu & html files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -24,5 +24,71 @@ angular.module('meanserviceApp', [
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .state('mylevel', {
+        url: '/mylevel',
+        templateUrl: 'views/mylevel.html',
+        controller: 'MainCtrl'
+      })
+      .state('myqna', {
+        url: '/myqna',
+        templateUrl: 'views/myqna.html',
+        controller: 'MainCtrl'
+      })
+      .state('myfollowing', {
+        url: '/myfollowing/{userId:[0-9]{1,4}}',
+        templateUrl: 'views/myfollowing.html',
+        controller: 'MainCtrl'
+      })
+      .state('techarea', {
+        url: '/techarea/{techId:[0-9]{1,4}}',
+        templateUrl: 'views/techarea.html',
+        controller: 'MainCtrl'
+      })
+      .state('gururanking', {
+        url: '/gururanking',
+        templateUrl: 'views/gururanking.html',
+        controller: 'MainCtrl'
+      })
+      .state('gurumember', {
+        url: '/gurumember',
+        templateUrl: 'views/gurumember.html',
+        controller: 'MainCtrl'
+      });
+
+  }])
+.run(['$rootScope', '$state', '$stateParams', '$http', '$location',
+  function ($rootScope, $state, $stateParams, $http, $location) {
+
+    /**
+     * @description
+     * It's very handy to add references to $state and $stateParams to the $rootScope
+     * so that you can access them from any scope within your applications.For example,
+     * <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
+     * to active whenever 'contacts.list' or one of its decendents is active.
+     */
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+
+    /**
+     * @description
+     * Reset error when a new view is loaded
+     */
+    $rootScope.$on('$viewContentLoaded', function() {
+
+    });
+
+    /**
+     * @description
+     * when we use 'ui-router' for routing, we must use $statChangeStart or $stateChangeSuccess
+     * instead of $routeChangeStart
+     */
+    $rootScope.$on('$stateChangeStart', function(evt, toState, toParams, fromState, fromParams) {
+
+    });
+  
 
   }]);
+
+
+
+
